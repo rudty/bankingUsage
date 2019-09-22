7d5e4a2f099e541464edb6ae49e399670856c8fa
rudty <rudtyz@gmail.com>
2019-09-22 18:16:57 +0900

    deviceListrepository jpa / 서비스에서 캐싱

--- src/main/kotlin/com/rudtyz/bank/repository/global/DeviceListRepository.kt
+++ src/main/kotlin/com/rudtyz/bank/repository/global/DeviceListRepository.kt
@@ -1,16 +1,7 @@
 package com.rudtyz.bank.repository.global
 
 import com.rudtyz.bank.model.Device
-import org.springframework.cache.annotation.Cacheable
-import org.springframework.stereotype.Repository
+import org.springframework.data.jpa.repository.JpaRepository
 
-@Repository
-class DeviceListRepository(
-    private val deviceListRepository: JpaDeviceListRepository
-):JpaDeviceListRepository by deviceListRepository {
+interface JpaDeviceListRepository: JpaRepository<Device, String>
 
-    @Cacheable("findAll")
-    override fun findAll(): MutableList<Device> {
-        return deviceListRepository.findAll()
-    }
-}
--- src/main/kotlin/com/rudtyz/bank/repository/global/JpaDeviceListRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.rudtyz.bank.repository.global
-
-import com.rudtyz.bank.model.Device
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface JpaDeviceListRepository: JpaRepository<Device, String>
-
